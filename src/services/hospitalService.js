e931824c3538e10dd28220d4db9e5ba787f3720a
ali-0111 <alirahanet@gmail.com>
2024-06-16 10:02:59 +0430

    Added controller and service for the hospital model

---
+++ src/services/hospitalService.js
@@ -0,0 +1,40 @@
+const prisma = require("#prismaClient");
+
+class HospitalService {
+  static async getAllHospitals() {
+    return await prisma.hospital.findMany();
+  }
+
+  static async getHospitalById(id) {
+    try {
+      return await prisma.hospital.findUnique({ where: { id: parseInt(id) } });
+    } catch (err) {
+      return null;
+    }
+  }
+
+  static async createHospital(data) {
+    return await prisma.hospital.create({ data });
+  }
+
+  static async updateHospital(id, data) {
+    try {
+      return await prisma.hospital.update({
+        where: { id: parseInt(id) },
+        data,
+      });
+    } catch (err) {
+      return null;
+    }
+  }
+
+  static async deleteHospital(id) {
+    try {
+      return await prisma.hospital.delete({ where: { id: parseInt(id) } });
+    } catch (err) {
+      return null;
+    }
+  }
+}
+
+module.exports = HospitalService;
