6681845ff3ce6b80df5432c729de1cef2c762be9
ali-0111 <alirahanet@gmail.com>
2024-07-12 23:59:52 +0430

    auth key renamed

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,21 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.token;
 
   if (!token) {
-    return res.status(403).json({ message: 'Token is required' });
+    return res.status(403).json({ message: "Token is required" });
   }
 
   jwt.verify(token, SECRET_KEY, (err, user) => {
-      if (err) {
-        return res.status(403).json({ message: 'Invalid token' });
-      }
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
 
-      req.user = user;
-      next();
+    req.user = user;
+    next();
   });
 };
 
